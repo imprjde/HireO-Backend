31ebe6e020c34083f00be89b8c3ad3ae746fd897
Prajwal <imprjde7@gmail.com>
2024-08-28 15:35:00 +0530

    update profle bug

--- index.js
+++ index.js
@@ -19,13 +19,9 @@ connectDB();
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-const corsOptions = {
-  origin: process.env.CORS_ORIGIN,
-  credentials: false,
-};
-
 // middleware
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(urlencoded({ extended: true }));
