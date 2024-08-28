99026c6fb62d3315e62beee66ad29f54d2fa30cf
Prajwal <imprjde7@gmail.com>
2024-08-28 10:22:01 +0530

    cors change

--- index.js
+++ index.js
@@ -20,16 +20,13 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 // middleware
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(cookieParser());
-const corsOptions = {
-  origin: "http://localhost:5173",
-  credentials: true,
-};
-app.use(cors(corsOptions));
+
 
 // api's route
 app.use("/api/v1/user", userRoute);
