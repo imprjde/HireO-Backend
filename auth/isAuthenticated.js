499de99e1ee745c0f1ecdda691c44181f04010b1
Prajwal <imprjde7@gmail.com>
2024-08-28 19:48:20 +0530

    Login Cookie changes

--- auth/isAuthenticated.js
+++ auth/isAuthenticated.js
@@ -1,3 +1,31 @@
+// import jwt from "jsonwebtoken";
+// const isAuthenticated = async (req, res, next) => {
+//   console.log("REQ FROM IS AUTH=", req.cookies.token);
+//   try {
+//     const token = req.cookies.token;
+//     if (!token) {
+//       return res.status(401).json({
+//         message: "User not authenticated",
+//         success: false,
+//       });
+//     }
+//     const decode = await jwt.verify(token, process.env.SECRET_KEY);
+//     if (!decode) {
+//       return res.status(401).json({
+//         message: "Invalid token",
+//         success: false,
+//       });
+//     }
+//     req.id = decode.userId;
+//     next();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+// export default isAuthenticated;
+
+///////////////////////////////////////////////////////////////////////
+
 import jwt from "jsonwebtoken";
 const isAuthenticated = async (req, res, next) => {
   console.log("REQ FROM IS AUTH=", req.cookies.token);
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -68,6 +68,59 @@ export const register = async (req, res) => {
   }
 };
 
+// export const login = async (req, res) => {
+//   try {
+//     const { email, password, role } = req.body;
+//     if (!email || !password || !role)
+//       return res
+//         .status(400)
+//         .json({ message: "All fields are required", success: false });
+//     let user = await User.findOne({ email });
+//     if (!user)
+//       return res.status(401).json({ message: "Incorrect email or password" });
+//     const isPasswordMatch = await bcrypt.compare(password, user.password);
+//     if (!isPasswordMatch)
+//       return res.status(401).json({ message: "Incorrect email or password" });
+
+//     if (role !== user.role) {
+//       return res
+//         .status(400)
+//         .json({ message: "Account doesn't exist with current role" });
+//     }
+//     const tokenData = {
+//       userId: user._id,
+//     };
+//     // const token = await jwt.sign(tokenData, process.env.SECRET_KEY, {
+//     //   expiresIn: "1d",
+//     // });
+//     const token = await jwt.sign(tokenData, process.env.SECRET_KEY);
+
+//     user = {
+//       _id: user._id,
+//       fullname: user.fullname,
+//       email: user.email,
+//       phoneNumber: user.phoneNumber,
+//       role: user.role,
+//       profile: user.profile,
+//     };
+//     return res
+//       .status(200)
+//       .cookie("token", token, {
+//         // maxAge: 1 * 24 * 60 * 60 * 1000,
+//         httpOnly: true,
+//         sameSite: "strict",
+//       })
+//       .json({
+//         message: `Welcome Back ${user.fullname}`,
+//         user,
+//         success: true,
+//       });
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(500).json({ message: "Server error", success: false });
+//   }
+// };
+
 export const login = async (req, res) => {
   try {
     const { email, password, role } = req.body;
@@ -106,9 +159,9 @@ export const login = async (req, res) => {
     return res
       .status(200)
       .cookie("token", token, {
-        // maxAge: 1 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "strict",
+        sameSite: "lax", // or "none" if using cross-site and HTTPS
+        secure: process.env.NODE_ENV === "production", // ensures cookie is only sent over HTTPS in production
       })
       .json({
         message: `Welcome Back ${user.fullname}`,
