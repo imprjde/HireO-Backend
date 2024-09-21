2c8a9fd15a840d08a5fd587fde7fe6e5fdcdf45c
Prajwal <imprjde7@gmail.com>
2024-09-21 20:20:36 +0530

    Push Notification Bug Fixxx

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,5 +1,8 @@
 // import admin from "firebase-admin";
-// import serviceAccount from "./firebaseAdminSDK.json" assert { type: "json" };
+// import { createRequire } from "module";
+// const require = createRequire(import.meta.url);
+
+// const serviceAccount = require("./firebaseAdminSDK.json");
 
 // admin.initializeApp({
 //   credential: admin.credential.cert(serviceAccount),
@@ -8,13 +11,16 @@
 // export default admin;
 
 import admin from "firebase-admin";
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-
-const serviceAccount = require("./firebaseAdminSDK.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+  }),
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
 });
-
 export default admin;
